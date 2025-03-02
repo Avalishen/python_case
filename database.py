0af22b939b0cfeef3db7351382ebf1fbacd4f767
Avalishen <121102829+Avalishen@users.noreply.github.com>
2025-03-02 21:57:05 +0300

    database.py

--- database.py
+++ database.py
@@ -3,20 +3,20 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "sqlite+aiosqlite:///./db.db"
 
-# Создаём асинхронный движок
+
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 
-# Создаём сессию
+
 AsyncSessionLocal = sessionmaker(
     bind=engine,
     class_=AsyncSession,
     expire_on_commit=False
 )
 
-# ✅ Определяем Base (чтобы Alembic знал про таблицы)
+
 Base = declarative_base()
 
-# Функция для получения сессии
+
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
